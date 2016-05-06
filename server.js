9c5c22bb58ad2ac15c31d8e235b3aa05c055cc4d
thierry@moncourriel.eu <thierry>
2016-05-06 10:16:53 +0200

    on revient a un truc basic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var $tw = require("tiddlywiki").TiddlyWiki();
 
 $tw.boot.argv = [
-  process.env.OPENSHIFT_DATA_DIR+fr,
+  process.env.OPENSHIFT_DATA_DIR,
   "--verbose",
   "--server",
   process.env.OPENSHIFT_NODEJS_PORT,
