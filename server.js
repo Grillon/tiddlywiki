e664b9b52124c288496df1574cb15f9510a077e4
thierry@moncourriel.eu <thierry>
2016-05-08 20:21:47 +0200

    ajout de la personnalisation de mot de passe
    Il est a present possible de configurer les logins mdp d'acces grace aux
    variables d'environnements TW_PASSWD et TW_USER
    
    rhc set-env TW_PASSWD=mon_pass TW_USER=mon_user /nom appli/
    puis rhc restart /nom appli/
    
    sinon au moment du deployement :
    
    app create tw nodejs-0.10 TW_USER=mon_user TW_PASSWD=mon_passwd --from-code=https://github.com/Grillon/tiddlywiki.git

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 var $tw = require("tiddlywiki").TiddlyWiki();
+  var mon_password = process.env.TW_PASSWD;
+  var mon_username = process.env.TW_USER;
+
+	if (mon_password === undefined) {
+		mon_password = "<password>";
+	}
+	if (mon_username === undefined) {
+		mon_username = "<username>";
+	}
+
 
 $tw.boot.argv = [
   process.env.OPENSHIFT_DATA_DIR,
@@ -8,8 +18,8 @@ $tw.boot.argv = [
   "$:/core/save/all",
   "text/plain",
   "text/html",
-  "<username>",
-  "<password>",
+  mon_username,
+  mon_password,
   process.env.OPENSHIFT_NODEJS_IP,
 ];
 +$tw.boot.boot();
